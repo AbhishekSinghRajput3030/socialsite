2c5a747f1b46d634ac7be1772e6bb76863b8cc2f
abhi-3030 <abhisheksingh.rajput2019@vitstudent.ac.in>
2021-09-22 03:59:24 +0530

    created view for home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,11 @@
 module.exports.home=function(req,res){
-    return res.end('<h1>Express is up for Codeial! </h1>')
+    /*we will removed this part of code bcz it sent something directly to the browser and 
+    now since we have created view we will rather render it than sending it directly */
+
+    //return res.end('<h1>Express is up for Codeial! </h1>') 
+
+    return res.render('home',{
+        title: "Home"
+    });
 }
 // module.exports.actionName = function(req,res) {}
