56c0ce739fc2706dc345ba0dd553840bdbcef380
abhi-3030 <abhisheksingh.rajput2019@vitstudent.ac.in>
2021-09-26 03:59:13 +0530

    dlete post and dlete comment fn added

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post')  //requiring posts schema
+const Comment = require('../models/comment');
 
 module.exports.create = function(req, res){ //creating post from the form in home.ejs
     Post.create({
@@ -6,7 +7,22 @@ module.exports.create = function(req, res){ //creating post from the form in hom
         user: req.user._id        //user._id is stored becz it is unique 
     }, function(err, post){
         if(err){console.log('error in creating a post'); return;}
-
         return res.redirect('back');
+    });
+}
+
+module.exports.destroy= function(req,res){
+    Post.findById(req.params.id,function(err,post){
+        //.id means converting the object id into string
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id},function(err){
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+
     });
 }
