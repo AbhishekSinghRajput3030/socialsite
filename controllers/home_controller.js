6464247c8a4a4e536015347622f01bb5917f87b4
abhi-3030 <abhisheksingh.rajput2019@vitstudent.ac.in>
2021-09-27 22:01:37 +0530

    update post and delete post without reloading

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,31 +1,34 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = async function (req, res) {
-    // populate the user of each post(check mongoose populate on google for refrence of code)
+
+
+module.exports.home = async function(req, res){
+
     try{
-        // populate the user of each post
-       let posts = await Post.find({})
-       .populate('user')
-       .populate({
-           path: 'comments',
-           populate: {
-               path: 'user'
-           }
-       });
+         // populate the user of each post
+        let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+    
+        let users = await User.find({});
+
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts,
+            all_users: users
+        });
+
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
    
-       let users = await User.find({});
-
-       return res.render('home', {
-           title: "Codeial | Home",
-           posts:  posts,
-           all_users: users
-       });
-
-   }catch(err){
-       console.log('Error', err);
-       return;
-   }
-  
 }
 // module.exports.actionName = function(req, res){}
