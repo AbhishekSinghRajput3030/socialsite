c7c916c4ef95e83a8b5da8f167b2891a6c18d06b
abhi-3030 <abhisheksingh.rajput2019@vitstudent.ac.in>
2021-10-14 22:32:27 +0530

    chenges in .json file

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,22 @@
 
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass');
 const cssnano = require('gulp-cssnano');
 const rev = require('gulp-rev');
-const uglify = require('gulp-uglify-es').default;  //use to minify the js
+const uglify = require('gulp-uglify-es').default;
 const imagemin = require('gulp-imagemin');
 const del = require('del');
 
 gulp.task('css', function(done){             //task to minify sccs
     console.log('minifying css...');
-    gulp.src('./assets/sass/**/*.scss')
+    gulp
+    .src('./assets/sass/**/*.scss')
     .pipe(sass())                        // to convert sass to css
     .pipe(cssnano())                    // to compress css
     .pipe(gulp.dest('./assets.css'));   //to put all of them in a folder
    //minification is done now we have to do naming i.e changing name
-    return gulp.src('./assets/**/*.css')
+    return gulp
+    .src('./assets/**/*.css')
     .pipe(rev())
     .pipe(gulp.dest('./public/assets'))
     .pipe(rev.manifest({
@@ -63,7 +65,7 @@ gulp.task('clean:assets', function(done){
 });
 
 //this built task is used to run all four of it
-gulp.task('build', gulp.series('clean:assets', 'css'), function(done){
+gulp.task('build', gulp.series('clean:assets', 'css','js', 'images'), function(done){
     console.log('Building assets');
     done();
 });
