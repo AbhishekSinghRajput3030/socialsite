832eb8f7ed19b0dbf9b3228311d4c2efa1e54154
abhi-3030 <abhisheksingh.rajput2019@vitstudent.ac.in>
2021-10-14 18:59:33 +0530

    type : common js to run es and imagemin

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ if (env.name =='development'){
 }
 
 
-app.use(express.urlencoded(extends));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
 app.use(express.static(env.asset_path));  //as path of asset is stored in environment.js
